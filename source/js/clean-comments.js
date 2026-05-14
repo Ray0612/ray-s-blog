b37bbce18848df5a85e3830dfa6e6328ffafed2a
Ray <ray@ray2.asia>
2026-05-15 00:21:59 +0800

    移除bilibili表情

--- source/js/clean-comments.js
+++ source/js/clean-comments.js
@@ -1,5 +1,4 @@
 setInterval(function(){
-  var a=document.querySelector('.tk-admin-actions');if(a)a.remove();
-  var i=document.querySelector('.tk-input-image');if(i)i.remove();
-  var m=document.querySelector('a[href*="guides.github.com"]');if(m)m.remove();
+  document.querySelectorAll('.tk-admin-actions,.tk-input-image,a[href*="guides.github.com"]').forEach(function(e){e.remove()});
+  document.querySelectorAll('.tk-tabs .tk-tab').forEach(function(e){if(e.textContent.includes('bilibili')||e.textContent.includes('Bilibili')||e.textContent.includes('B站'))e.remove()});
 },500);
