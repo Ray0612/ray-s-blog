c6ec76745ad1e144074513f79c303f3cd408d081
潘瑞 <ray@Rays-Mac-mini.local>
2026-05-15 00:20:17 +0800

    JS清理评论区按钮

---
+++ source/js/clean-comments.js
@@ -0,0 +1,5 @@
+setInterval(function(){
+  var a=document.querySelector('.tk-admin-actions');if(a)a.remove();
+  var i=document.querySelector('.tk-input-image');if(i)i.remove();
+  var m=document.querySelector('a[href*="guides.github.com"]');if(m)m.remove();
+},500);
